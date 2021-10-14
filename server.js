41ea9d58d906eb3b9ceaa0411f5023e12d0a01f0
Lucas Di Biase <lucas.dibiase@gmail.com>
2021-10-14 13:06:02 -0300

    Revert

--- server.js
+++ server.js
@@ -19,14 +19,6 @@ app.use('/users', usersRouter);
 
 // Listen from process.env.PORT
 
-var server_port = 3000 || process.env.PORT || 80;
-var server_host = '0.0.0.0';
-app.listen(server_port, server_host, function() {
-  console.log(`Server started on port: http://localhost:${process.env.PORT}, DB: ${process.env.DATABASE_URL}`)
-});
-
-/*
 app.listen(process.env.PORT || 3000, () =>
   console.log(`Server started on port: http://localhost:${process.env.PORT}, DB: ${process.env.DATABASE_URL}`)
 );
-*/
