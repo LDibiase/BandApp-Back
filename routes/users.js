8a402ef711c8bc2427d8b2bea600039a5af1b4c8
Lucas Di Biase <lucas.dibiase@gmail.com>
2021-10-25 15:18:36 -0300

    More entrypoints and fixes

--- routes/users.js
+++ routes/users.js
@@ -15,24 +15,76 @@ router.post('/', (req, res) => {
         firstname: req.body.firstname,
         surname: req.body.surname,
         email: req.body.email,
-        password: req.body.password,
-        instruments: [],
-        genres: [],
-        groups: [],
+        password: req.body.password
     }
     
     dbService.getDataBase().ref('users/' + req.body.username).set(usr);
+
     res.json("Post success");
 });
 
-// PUT
+// PUT CAMBIO CONTRASEÑA
 router.put('/', (req, res) => {
     const usr = {
         password: req.body.password,
     }
     
-    dbService.getDataBase().ref('users/' + req.body.username).update(usr)
+    dbService.getDataBase().ref('users/' + req.body.username).update(usr);
     res.json("Put success")
+});
+
+// PUT INSTRUMENT
+router.put('/instrument', (req, res) => {
+    dbService.getDataBase().ref('users/' + req.body.username + '/instruments/' + req.body.instrument).push(req.body.instrument);
+
+    res.json("Put success")
+});
+
+// PUT FRIEND
+router.put('/instrument', (req, res) => {
+    dbService.getDataBase().ref('users/' + req.body.username + '/friends/' + req.body.friend).push(req.body.friend);
+
+    res.json("Put success")
+});
+
+// PUT GENRE
+router.put('/instrument', (req, res) => {
+    dbService.getDataBase().ref('users/' + req.body.username + '/genres/' + req.body.genre).push(req.body.genre);
+
+    res.json("Put success")
+});
+
+// GET USER INSTRUMENTS
+router.get('/:username/instruments', (req, res) => {
+    const ref = dbService.getDataBase().ref('users/' + req.params.username + '/instruments');
+
+    ref.on('value', (snapshot) => {
+        res.json(snapshot.val());
+      }, (errorObject) => {
+        console.log('The read failed: ' + errorObject.name);
+      });
+});
+
+// GET USER FRIENDS
+router.get('/:username/friends', (req, res) => {
+    const ref = dbService.getDataBase().ref('users/' + req.params.username + '/friends');
+
+    ref.on('value', (snapshot) => {
+        res.json(snapshot.val());
+      }, (errorObject) => {
+        console.log('The read failed: ' + errorObject.name);
+      });
+});
+
+// GET USER GENRES
+router.get('/:username/genres', (req, res) => {
+    const ref = dbService.getDataBase().ref('users/' + req.params.username + '/genres');
+
+    ref.on('value', (snapshot) => {
+        res.json(snapshot.val());
+      }, (errorObject) => {
+        console.log('The read failed: ' + errorObject.name);
+      });
 });
 
 // GET BY USERNAME
